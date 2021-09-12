2f86e0d2a7331a43504d5a374e7393a7ac67d0e8
DBotThePony <dbotthepony@yandex.ru>
2021-09-12 10:24:02 +0700

    Make it use async :tiny_potato:

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-let http = require("http");
-let fs = require("fs");
-let sidebarPath = "./sidebar.json";
-let htmlPath = "./index.html";
-let toSidebar = require("./sidebar").toSidebar;
-http.createServer(function (req, res) {
+
+const http = require("http");
+const fs = require("fs").promises;
+const sidebarPath = "./sidebar.json";
+const htmlPath = "./index.html";
+const toSidebar = require("./sidebar").toSidebar;
+
+http.createServer(async function (req, res) {
     if (req.url.startsWith("/git")) {
         res.writeHead(307, {Location: "http://github.com/oliver-makes-code" + req.url.substr(4)});
         res.end();
@@ -15,30 +17,42 @@ http.createServer(function (req, res) {
         res.end();
         return;
     }
+
     let loc = "." + req.url;
+
     if (req.url.startsWith("/assets")) {
-        if (!fs.existsSync(loc)) {
+        try {
+            await fs.stat(loc);
+        } catch(err) {
             res.writeHead(404);
             res.end();
             return;
         }
+
         res.writeHead(200);
-        res.write(fs.readFileSync(loc));
+        res.write(await fs.readFile(loc));
         res.end();
+
         return;
     }
-    loc = "./site"+req.url;
-    if (!fs.existsSync(loc+"/title.txt") || !fs.existsSync(loc+"/body.txt")) {
+
+    loc = "./site" + req.url;
+
+    try {
+        await fs.stat(loc + "/title.txt");
+        await fs.stat(loc + "/body.txt");
+    } catch(err) {
         res.writeHead(404);
         res.end();
         return;
     }
-    let title = fs.readFileSync(loc+"/title.txt").toString();
-    let content = fs.readFileSync(loc+"/body.txt").toString();
-    let sidebar = toSidebar(JSON.parse(fs.readFileSync(sidebarPath)));
-    let html = fs.readFileSync(htmlPath).toString().replace("%sidebar%",sidebar).replace("%content%",content).replace("%title%",title);
+
+    const title = await fs.readFile(loc+"/title.txt", {encoding: 'utf-8'});
+    const content = await fs.readFile(loc+"/body.txt", {encoding: 'utf-8'});
+    const sidebar = toSidebar(JSON.parse(await fs.readFile(sidebarPath, {encoding: 'utf-8'})));
+    const html = (await fs.readFile(htmlPath, {encoding: 'utf-8'})).replace("%sidebar%", sidebar).replace("%content%", content).replace("%title%", title);
+
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(html);
     res.end();
-
 }).listen(80);
